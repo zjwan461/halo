cdf481b473f821ff33164ceccd87919cad4cfa47
Jerry Su <826935261@qq.com>
2021-09-26 21:05:01 +0800

    添加服务器判断

--- src/main/java/run/halo/app/service/impl/ServerServiceImpl.java
+++ src/main/java/run/halo/app/service/impl/ServerServiceImpl.java
@@ -1,6 +1,7 @@
 package run.halo.app.service.impl;
 
 import java.util.Map;
+import org.apache.commons.lang3.SystemUtils;
 import org.springframework.stereotype.Service;
 import run.halo.app.model.dto.ServerInfoDTO;
 import run.halo.app.service.ServerService;
@@ -14,9 +15,14 @@ import run.halo.app.utils.LinuxStateForShell;
 public class ServerServiceImpl implements ServerService {
     @Override
     public ServerInfoDTO getInfo() {
-        Map<String, String> result =
-            LinuxStateForShell.runDistanceShell(LinuxStateForShell.COMMANDS, "devuser", "asdf1111",
+        Map<String, String> result = null;
+        if (SystemUtils.IS_OS_LINUX) {
+            result = LinuxStateForShell.runLocalShell(LinuxStateForShell.COMMANDS);
+        } else {
+            result = LinuxStateForShell.runDistanceShell(LinuxStateForShell.COMMANDS, "devuser",
+                "asdf1111",
                 "156.247.9.176");
+        }
         return LinuxStateForShell.getServerInfo(result);
     }
 }
